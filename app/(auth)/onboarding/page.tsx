b8bd1b48a7d94a6dd3f3520c8f77d4ad75669960
robert-devs <mainrobert04@gmail.com>
2024-02-15 12:56:38 +0300

    set-up and initilization

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,34 @@
+import { currentUser } from "@clerk/nextjs";
+
+async function Page() {
+	// const user = await currentUser();
+	// console.log(user);
+
+	const userInfo = {};
+
+	// const userData = {
+	//     id: user?.id,
+	//     objectId: userInfo?._id,
+	//     username: userInfo?.username || user?.username,
+	//     name: userInfo?.name || user?.firstName || '',
+	//     bio: userInfo?.bio || "",
+	//     Image: userInfo?.image || user?.imageUrl
+	// };
+	return (
+		<main className="flex flex-col justify-start max-w-3xl px-10 py-20 mx-auto">
+			<h1 className="head-text">onboarding</h1>
+			<p className="mt-3 text-base-regular text-light-1">
+				complete yur profile now to use threads
+			</p>
+
+			<section className="p-10 mt-9 bg-dark-2">
+				{/* <AccountProfile
+                    // user={userData}
+                    btnTitle="continues"
+                /> */}
+			</section>
+		</main>
+	);
+}
+
+export default Page;
